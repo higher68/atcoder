c8f6acfd1dd21057647071904e9b08e6107d4c38
higher68 <hatiro7code@gmail.com>
2018-07-13 07:50:14 +0900

    20180713_trueans

---
+++ Beginner093/b2.py
@@ -0,0 +1,11 @@
+a, b, k = map(int, input().split())
+x = []
+lx = b-a+1
+if k <= lx//2:
+    for i in range(a, a+k):
+        print(i)
+    for i in range(b-k+1, b+1):
+        print(i)
+else:
+    for i in range(a, b+1):
+        print(i)
