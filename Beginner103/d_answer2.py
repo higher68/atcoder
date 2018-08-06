83de7ff6413305d6ac77b9660af36fe143add44d
higher68 <hatiro7code@gmail.com>
2018-08-06 21:10:25 +0900

    20180806_processing

--- Beginner103/d_answer2.py
+++ Beginner103/d_answer2.py
@@ -3,15 +3,16 @@ n, m = map(int, input().split())
 x = [[0, 0] for i in range(m)]
 for i in range(m):
     x[i][0], x[i][1] = map(int, input().split())
-x = sorted(x, key = lambda y: y[0])
+x = sorted(x, key=lambda y: y[0])
 ans = 1
-mx = max(x[0])
+mx = max([x[i][0] for i in range(m)])
 while len(x) > 0:
-    for i in range(len(x)):
-        if mx <= x[i][1]:
+    for i in range(len(x)-1, -1, -1):
+        if mx < x[i][1]:
             x.pop(i)
+            break
         else:
             mx = x[i][0]
             ans += 1
             break
-prin(ans)
+print(ans)
