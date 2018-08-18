42634bc28d6b9adea93cd3006a3f4f7700e59546
junya <hatiro7code@gmail.com>
2018-08-18 18:57:05 +0900

    20180818-sort-correct-AC

---
+++ Beginner094/c_answer2.py
@@ -0,0 +1,26 @@
+# sortはネックになる
+n = int(input())
+x_tmp = input().split()
+x = [[0 for j in range(3)] for i in range(n)]
+for i in range(n):
+    x[i][0] = int(x_tmp[i])
+    x[i][1] = i
+# print(x)
+# sortは先に済ませておく
+# for i in range(n):
+    # print(x[i])
+    # print('-'*20)
+x = sorted(x, key=lambda y: y[0])
+# keyで指定すると、各行の2列目でsortしちゃう
+# for i in range(n):
+    # print(x[i])
+# print(x)
+mid = n//2
+for i in range(n):
+    if i <= mid-1:
+        x[i][2] = x[mid][0]
+    else:
+        x[i][2] = x[mid-1][0]
+x = sorted(x, key=lambda y: y[1])
+for i in range(n):
+    print(x[i][2])
