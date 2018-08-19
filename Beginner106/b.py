b157e758c6fb23ef93fbb84ef4df473df6f755ff
junya <hatiro7code@gmail.com>
2018-08-19 12:05:07 +0900

    AC but 2accident, need review

---
+++ Beginner106/b.py
@@ -0,0 +1,13 @@
+n = int(input())
+if n < 105:
+    print(0)
+elif 105 <= n < 135:
+    print(1)
+elif 135 <= n < 165:
+    print(2)
+elif 165 <= n < 189:
+    print(3)
+elif 189 <= n < 195:
+    print(4)
+elif 195 <= n:
+    print(5)
