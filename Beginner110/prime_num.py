c7125a1e33cfc894e77ed992124a8b0971fbf810
junya <hatiro7code@gmail.com>
2018-10-15 08:06:38 +0900

    2018105 wrote output primenumbers

---
+++ Beginner110/prime_num.py
@@ -0,0 +1,25 @@
+def prime_num(max_input):
+    data = [1] * max_input
+    result = [0] * max_input
+    m = 2
+    n = 0
+    result[n] = m
+    data[0] = 0
+    data[1] = 0
+    while 1:
+        print(m)
+        if data[m] == 1:
+            i = 2 * m
+            while i <= max_input:
+                data[i] = 0
+                i += m
+            result[n] = m
+            n += 1
+            m += 1
+        else:
+            m += 1
+        if m >= max_input:
+            return result
+
+
+print(prime_num(11))
