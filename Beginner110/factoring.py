5dc1aa487670547dd3ecc238a1fc67f05d392982
junya <hatiro7code@gmail.com>
2018-10-16 08:54:29 +0900

    20181016 wrote but need debug

--- Beginner110/factoring.py
+++ Beginner110/factoring.py
@@ -1,17 +1,29 @@
-import prime_num
+from prime_num import prime_num
+from collections import OrderedDict
 
 
 def factoring(n):
-    i = 0
-    j = 0
     result = prime_num(n)
+    print(result)
     loop_len = len(result)
-    pf = {}
+    pf = OrderedDict()
     for i in range(0, loop_len):
         num = 0
         while 1:
             if n % result[i] == 0:
+                n /= result[i]
                 if num == 0:
                     pf[result[i]] = 1
                 else:
+                    print(pf[result[i]])
                     pf[result[i]] += 1
+                    print(pf[result[i]])
+                    num += 1
+            else:
+                break
+        if n == 1:
+            return pf
+
+
+fact = factoring(12)
+print(fact)
