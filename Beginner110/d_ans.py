16e5c7877d50587b2e96b0f1053d3f2e1ca7821f
junya <hatiro7code@gmail.com>
2018-10-20 23:35:52 +0900

    20181018 TLE need optimization

--- Beginner110/d_ans.py
+++ Beginner110/d_ans.py
@@ -1,4 +1,13 @@
 from factoring import factoring
 from collections import OrderedDict
+from itertools import combinations
 
 n, m = map(int, input().split())
+
+fact_list = list(factoring(m).values())
+
+sol = 1
+for val in fact_list:
+    sol *= len(list(combinations(range(val+n-1), val)))
+
+print(sol)
