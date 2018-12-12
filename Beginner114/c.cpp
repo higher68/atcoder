e7b2c44dd3768fe854713969da0b6b12470affea
junya <hatiro7code@gmail.com>
2018-12-12 23:51:47 +0900

    20181212 writing

---
+++ Beginner114/c.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int dfs(string s) {
+  if (int(s) > N) {
+    return 0;
+  } 
+  if all{
+  ret = 1;
+  }
+  string c = '753';
+  for (int i=0;i<3;i++){
+    ret += dfs(s+c[i]);
+  }
+  return ret;
+
+int main() {
+  cout << dfs('0') << endl;
+}
+
