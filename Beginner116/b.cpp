af896b08248fe176a73555746c8bda87f32df163
junya <hatiro7code@gmail.com>
2019-01-27 23:56:19 +0900

    writing

---
+++ Beginner116/b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int func(int n)
+{
+    if (n % 2 == 0)
+    {
+        return n / 2;
+    }
+    else
+    {
+        return 3 * n + 1;
+    }
+    return 0;
+}
+
+int main()
+{
+    int s;
+    cin >> s;
+
+    return 0;
+}
