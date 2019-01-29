3d2573a55190b814be4bee0dccac67d8d7f869d8
junya <hatiro7code@gmail.com>
2019-01-29 23:49:29 +0900

    AC、lint調整

--- Beginner116/b.cpp
+++ Beginner116/b.cpp
@@ -4,14 +4,10 @@ static const int MAX = 1000000;
 
 int a[MAX] = {0};
 
-int func(int n)
-{
-    if (n % 2 == 0)
-    {
+int func(int n) {
+    if (n % 2 == 0) {
         return n / 2;
-    }
-    else
-    {
+    } else {
         return 3 * n + 1;
     }
     return 0;
@@ -24,19 +20,14 @@ int main()
     cin >> s;
     a_tmp = s;
     a[a_tmp - 1] = 1;
-    for (int i = 1; i <= MAX; i++)
-    {
+    for (int i = 1; i <= MAX; i++) {
         a_tmp = func(a_tmp);
-        if (a[a_tmp - 1] == 1)
-        {
-            cout << a_tmp;
+        if (a[a_tmp - 1] == 1) {
+            cout << i + 1;
             break;
-        }
-        else
-        {
+        } else {
             a[a_tmp - 1] = 1;
         }
     }
-
     return 0;
 }
