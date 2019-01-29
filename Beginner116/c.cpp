cafcf83b759463bfa801030df2f1eaa65d2b7def
junya <hatiro7code@gmail.com>
2019-01-30 08:24:25 +0900

    ロジック作成中

--- Beginner116/c.cpp
+++ Beginner116/c.cpp
@@ -1,2 +1,23 @@
 #include<iostream>
 using namespace std;
+static const int MAX=100;
+
+int min_serve(int *h) {
+    int result;
+    result = 'hog';
+    return result;
+}
+
+
+int main() {
+    int N;
+    int h[MAX] = {0};
+    cin >> N;
+    for(int i=0; i<N; i++) {
+        cin >> h[i];
+    }
+    return min_serve(h);
+
+    return 0;
+
+}
