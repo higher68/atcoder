a07fa85b3232788ba2b5becd9fc87a63a9a38653
junya <hatiro7code@gmail.com>
2019-01-31 23:56:42 +0900

    アルゴリズム決定。実装。

--- Beginner116/c.cpp
+++ Beginner116/c.cpp
@@ -1,17 +1,28 @@
 #include<iostream>
 using namespace std;
 static const int MAX=100;
+int h[MAX] = {0};
 
-int min_serve(int *h) {
+int min_serve(int start, int stop) {
     int result;
-    result = 'hog';
+    bool all_one = true;
+    for(int i = start; i <= stop; i++) {
+        if(h[i] == 0){
+            all_one = false;
+            break;
+        }
+    }
+    if(all_one ==true) {
+        for(int i = start; i <= stop; i++) {
+            h[i] -= 1;
+        }
+    }
+
     return result;
 }
 
-
 int main() {
     int N;
-    int h[MAX] = {0};
     cin >> N;
     for(int i=0; i<N; i++) {
         cin >> h[i];
