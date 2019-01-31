9e5b69288ebd04725c96adffd971ba743430073a
junya <hatiro7code@gmail.com>
2019-02-01 01:16:29 +0900

    実装。詳細詰め中

--- Beginner116/c.cpp
+++ Beginner116/c.cpp
@@ -1,24 +1,38 @@
 #include<iostream>
 using namespace std;
 static const int MAX=100;
-int h[MAX] = {0};
+int h[MAX+1] = {0};
+int sol = 0;
 
 int min_serve(int start, int stop) {
     int result;
     bool all_one = true;
+    int tmp_start1, tmp_start2;
     for(int i = start; i <= stop; i++) {
-        if(h[i] == 0){
-            all_one = false;
+        if(h[i] > 0){
+            tmp_start1 = i;
             break;
         }
     }
+    cout <<  tmp_start1 << endl;
+    tmp_start2 = tmp_start1;
+    for(int i = tmp_start1; i <= stop;i++) {
+        if(h[i] == 0){
+            all_one = false;
+            min_serve(tmp_start2, i-1);
+            tmp_start2 = i + 1;
+        }
+    }
+    cout << all_one;
     if(all_one ==true) {
+        cout << 33333 << endl;
         for(int i = start; i <= stop; i++) {
             h[i] -= 1;
         }
+        sol += 1;
+        min_serve(start, stop);
     }
-
-    return result;
+    return sol;
 }
 
 int main() {
@@ -27,8 +41,7 @@ int main() {
     for(int i=0; i<N; i++) {
         cin >> h[i];
     }
-    return min_serve(h);
+    return min_serve(0, N-1);
 
     return 0;
-
 }
