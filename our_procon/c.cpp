ff1191b370e23601ae05665964be24fdcc1757d3
junya <hatiro7code@gmail.com>
2019-02-11 00:16:10 +0900

    AC

--- our_procon/c.cpp
+++ our_procon/c.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    int K, A, B;cin >> K >> A >> B;
-    int sol = 0;
+    long K, A, B;cin >> K >> A >> B;
+    long sol = 0;
     if(B-A <=2) {
         cout << K+1;
     } else {
         if(K <= A){
             cout << K+1;
         } else {
-            int diff = K-A+1;
+            long diff = K-A+1;
             sol = A;
             if(diff % 2 == 0){
-                sol = (K-B)*diff/2;
+                sol += (B-A)*diff/2;
             } else {
-                sol = (K-B)*(diff-1)/2+1;
+                sol += (B-A)*(diff-1)/2+1;
             }
             cout << sol;
         }
