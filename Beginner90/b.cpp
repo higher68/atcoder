f695bbd4cea7df5b62bc15a9bab55468e6ed8f60
junya <hatiro7code@gmail.com>
2019-02-13 00:42:46 +0900

    AC。ただし愚直法。要回答参照

--- Beginner90/b.cpp
+++ Beginner90/b.cpp
@@ -3,43 +3,69 @@ using namespace std;
 int main(void) {
     int A, B; cin >>A >> B;
     int count = 0;
-    for(int i=0;i<10;i++) {
+    //cout << "XXXXX" << endl;
+    for(int i=1;i<10;i++) {
         int candi=i*10000 + i * 1000+i*100+i*10+i;
+        //cout << candi << endl;
         if(A <= candi and candi <= B) {
-            count += 1
+            //cout << candi << "dayo" << endl;
+            count += 1;
         }
     }
-    for(int i=0;i<10;i++) {
-        for (j 0;j<1;j++){
+    //cout << "XXYXX" << endl;
+    for(int i=1;i<10;i++) {
+        for (int j=0;j<10;j++){
             if (i != j){
-                int candi = i*10000 + j * 1000+i*100+i*10+i;
+                int candi = i*10000 + i * 1000+j*100+i*10+i;
+                //cout << candi << endl;
                 if(A <= candi and candi <= B) {
-                    count += 1
+                    //cout << candi << "dayo" << endl;
+                    count += 1;
                 }
             }
         }
     }
-    for(int i=0;i<10;i++) {
-        for (j 0;j<1;j++){
+    //cout << "XYYYX" << endl;
+    for(int i=1;i<10;i++) {
+        for (int j=0;j<10;j++){
             if (i != j){
-                int candi = j*10000 + j * 1000+j*100+i*10+i;
+                int candi = i*10000 + j * 1000+j*100+j*10+i;
+                //cout << candi << endl;
                 if(A <= candi and candi <= B) {
-                    count += 1
+                    //cout << candi << "dayo" << endl;
+                    count += 1;
                 }
             }
         }
     }
-    for(int i=0;i<10;i++) {
+    //cout << "XZYZX" << endl;
+    for(int i=1;i<10;i++) {
         for (int j=0;j<10;j++){
             for(int k=0; k<10;k++){
                 if (i != j and k != j and k !=i){
-                    int candi = j*10000 + k * 1000+j*100+k*10+i;
+                    int candi = i*10000 + k * 1000+j*100+k*10+i;
+                    //cout << candi << endl;
                     if(A <= candi and candi <= B) {
-                        count += 1
+                        //cout << candi << "dayo" << endl;
+                        count += 1;
                     }
                 }
             }
         }
     }
+    //cout << "XYXYX" << endl;
+    for(int i=1;i<10;i++) {
+        for (int j=0;j<10;j++){
+            if (i != j){
+                int candi = i*10000 + j * 1000+i*100+j*10+i;
+                //cout << candi << endl;
+                if(A <= candi and candi <= B) {
+                    //cout << candi << "dayo" << endl;
+                    count += 1;
+                }
+            }
+        }
+    }
+    cout << count << endl;
     return 0;
 }
