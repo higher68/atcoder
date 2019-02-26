2535da611628cd6c9b92f65e2aacca1b07924194
junya <hatiro7code@gmail.com>
2019-02-26 23:52:21 +0900

    writing dfs

---
+++ Beginner119/c_sol.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;oct
+
+void dfs(int depth, vector<int> v){
+    if(depth=N) calc(v)
+    for(int i=0;i<<N;i++){
+        v[depth]=i;
+        dfs(depth+1, v);
+    }
+}
+
+int main(void) {
