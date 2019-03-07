06319551477be3356cd5b96335fc115c4d415e1e
junya <hatiro7code@gmail.com>
2019-03-07 23:47:15 +0900

    writing

---
+++ Beginner120/c.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+string S;
+int sol=0;
+void calc(sol){
+    judge = 0;
+    for(int i=1;i<strlen(S.c_str());i+=2){
+        pre = int(S[i-1]);
+        back = int(S[i]);
+        if(abs(back-pre))
+    }
+
+}
+int main(void){
+    cin >> S;
+    calc(S)
+    return 0;
+}
