e3c0c4a69c2aee1054fe3d94d6c02e222cfe7912
junya <hatiro7code@gmail.com>
2019-03-13 00:14:43 +0900

    writing dp

---
+++ Beginner180/d_sol.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int func(int i)
+{
+    vector<int> A{2, 5, 5, 4, 5, 6, 3, 7, 6};
+    return A[i - 1];
+}
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(M);
+    for (int i = 0; i < M; i++)
+    {
+        cin >> A[i];
+    }
+    vector<int> dp(N, 0);
+    for (int i = 1; i < N; i++)
+    {
+        int mx = dp[i - func(A[0] + 1)];
+        for (int j = 1; j < M; j++)
+        {
+            int mx2 = dp[i - func(A[j]) + 1];
+            mx = max(mx, mx2);
+        }
+        dp[i] = mx;
+    }
+    cout << dp[N];
+    return 0;
+}
