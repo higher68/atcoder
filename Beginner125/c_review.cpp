d53d9a1e3dcdbecf58a47fa18fb8e865e0b59ab0
junya <hatiro7code@gmail.com>
2019-05-09 00:14:07 +0900

    復習 AC

--- Beginner125/c_review.cpp
+++ Beginner125/c_review.cpp
@@ -32,32 +32,33 @@ int main()
         left_sum[i] = gcd(left_sum[i - 1], A[i]);
         right_sum[i] = gcd(right_sum[i - 1], A[N - i - 1]);
     }
-    for (int i = 0; i < N; i++)
-    {
-        cout << "left_sum" << left_sum[i] << endl;
-        cout << "rightsum" << right_sum[i] << endl;
-    }
+    reverse(right_sum.begin(), right_sum.end());
+    // for (int i = 0; i < N; i++)
+    // {
+    //     cout << "left_sum" << left_sum[i] << endl;
+    //     cout << "right_sum" << right_sum[i] << endl;
+    // }
     vector<int> calc(N);
     for (int i = 0; i < N; i++)
     {
         if (i == 0)
         {
-            calc[i] = right_sum[N - i - 1];
+            calc[0] = right_sum[1];
         }
         else if (i == N - 1)
         {
-            calc[i] = left_sum[i - 1];
+            calc[N - 1] = left_sum[N - 2];
         }
         else
         {
-            calc[i] = gcd(left_sum[i - 1], right_sum[N - i - 1]);
+            calc[i] = gcd(left_sum[i - 1], right_sum[i + 1]);
         }
     }
     int ans = *max_element(calc.begin(), calc.end());
-    for (int i = 0; i < N; i++)
-    {
-        cout << calc[i] << endl;
-    }
+    //for (int i = 0; i < N; i++)
+    //{
+    //    cout << "calc" << calc[i] << endl;
+    //}
     cout << ans << endl;
     return 0;
 }
