582f61c40e4f28bb2e61213e108c985e77dcedba
junya <hatiro7code@gmail.com>
2019-05-14 23:19:11 +0900

    AC 復習 charにキャスト

---
+++ Beginner124/c_review2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string S;
+    cin >> S;
+    int ans = S.size();
+    for (int i; i < 2; i++)
+    {
+        int cnt = 0;
+        for (int j = 0; j < S.size(); j++)
+        {
+            if (j % 2 == 0 && S[j] == char(i + '0'))
+            {
+                cnt++;
+            }
+            if (j % 2 != 0 && S[j] != char(i + '0'))
+            {
+                cnt++;
+            }
+        }
+        ans = min(ans, cnt);
+    }
+    cout << ans << endl;
+    return 0;
+}
